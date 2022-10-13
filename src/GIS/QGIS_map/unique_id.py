e0ef1806543bd00a46b2e053710bc4212d07c0ef
n-raspi <n.seemannricard@gmail.com>
2022-10-13 09:26:48 +0200

    updates

---
+++ src/GIS/QGIS_map/unique_id.py
@@ -0,0 +1,6 @@
+from PyQt5.QtCore import QVariant
+import pandas as pd
+import os
+cwd = os.getcwd()
+print(cwd)
+#df_mzedi = pd.read_csv('../../../data/interm_data/mzedi_entry_skip_data_stats.csv')
